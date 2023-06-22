d9a338940d404de550d7faa561116f0786e066a7
Rahulzhp <akumari42872@gmail.com>
2023-06-22 20:52:24 +0530

    created contentfile

---
+++ frontend/src/Component/Content.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import "./Main.css"
+import { BsArrowRightShort } from "react-icons/bs"
+
+
+
+const Content = ({ prop }) => {
+    console.log(prop)
+    return (
+        <div className="ContentArea">
+            <div>
+                <div>
+                    <h1>Company Information</h1>
+                    <div>
+                        <div>
+                            <p>Operating company</p>
+                            <h3>{prop.Operating_Company}</h3>
+                        </div>
+
+                        <div>
+                            <p>Company type</p>
+                            <h3>{prop.Company_type}</h3>
+                        </div>
+                        <div>
+                            <p>Law firm or buisness name</p>
+                            <h3>{prop.law}</h3>
+                        </div>
+                    </div>
+                    <div id="div">
+                        <p>Address 1</p>
+                        <h3>{prop.Adress1}</h3>
+                    </div>
+
+                    <div>
+                        <div>
+                            <p>State</p>
+                            <h3>{prop.state}</h3>
+                        </div>
+
+                        <div>
+                            <p>City</p>
+                            <h3>{prop.city}</h3>
+                        </div>
+                        <div>
+                            <p>County</p>
+                            <h3>{prop.country}</h3>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <div>
+                            <p>Zip code</p>
+                            <h3>{prop.zip}</h3>
+                        </div>
+
+                        <div>
+                            <p>Phone number</p>
+                            <h3>{prop.phone}</h3>
+                        </div>
+
+                    </div>
+                </div>
+                <div>
+                    <h1>Law firm information</h1>
+                    <div>
+                        <div>
+                            <p>Firm speciality</p>
+                            <h3>{prop.FS}</h3>
+                        </div>
+                        <div>
+                            <p>Case managment</p>
+                            <h3>{prop.Case_mangement}</h3>
+                        </div>
+                    </div>
+                    <div id="div">
+                        <p>Account manager</p>
+                        <h3>{prop.Account_manager}</h3>
+                    </div>
+                </div>
+                <div>
+                    <h1>Company notification settings</h1>
+                    <div>
+                        <div>
+                            <p>New user invited</p>
+                            <h3>{prop.New_User_Inv}</h3>
+                        </div>
+                        <div>
+                            <p>New user added</p>
+                            <h3>{prop.New_User_added}</h3>
+                        </div>
+                        <div>
+                            <p>Payment method changed</p>
+                            <h3>{prop.Payment_mail}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="button">
+                    <button>Next <BsArrowRightShort size={21} /></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content
--- frontend/src/Component/Main.tsx
+++ frontend/src/Component/Main.tsx
@@ -5,6 +5,7 @@ import bell from "../Icons/Group4.png"
 
 import profile from "../Icons/Group3.png";
 import axios from 'axios';
+import Content from './Content';
 
 const Main = () => {
     const [data, setData] = useState([])
@@ -41,7 +42,7 @@ const Main = () => {
                     </div>
                     <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                         <div >
-                            <img src={bell} style={{ width: "71px", }}></img>
+                            <img src={bell} style={{ width: "71px", marginRight: "9px" }}></img>
                         </div>
                         <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
                             <img
@@ -54,99 +55,10 @@ const Main = () => {
                     </div>
                 </nav>
                 {/* Body content goes here */}
-                <div className="ContentArea">
-                    <div>
-                        <div>
-                            <h1>Company Information</h1>
-                            <div>
-                                <div>
-                                    <p>Operating company</p>
-                                    <h3>Pro Legal Serve</h3>
-                                </div>
+                {
+                    data && data.map((el) => <Content prop={el} key={el.id}/>)
+                }
 
-                                <div>
-                                    <p>Company type</p>
-                                    <h3>B2B</h3>
-                                </div>
-                                <div>
-                                    <p>Law firm or buisness name</p>
-                                    <h3>Flash</h3>
-                                </div>
-                            </div>
-                            <div id="div">
-                                <p>Address 1</p>
-                                <h3>Company Address</h3>
-                            </div>
-
-                            <div>
-                                <div>
-                                    <p>State</p>
-                                    <h3>State</h3>
-                                </div>
-
-                                <div>
-                                    <p>City</p>
-                                    <h3>City</h3>
-                                </div>
-                                <div>
-                                    <p>County</p>
-                                    <h3>County</h3>
-                                </div>
-                            </div>
-
-
-                            <div>
-                                <div>
-                                    <p>Zip code</p>
-                                    <h3>Zip code</h3>
-                                </div>
-
-                                <div>
-                                    <p>Phone number</p>
-                                    <h3>1234567890</h3>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div>
-                            <h1>Law firm information</h1>
-                            <div>
-                                <div>
-                                    <p>Firm speciality</p>
-                                    <h3>Family Law</h3>
-                                </div>
-                                <div>
-                                    <p>Case managment</p>
-                                    <h3>Server Manager</h3>
-                                </div>
-                            </div>
-                            <div id="div">
-                                <p>Account manager</p>
-                                <h3>Account manager</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <h1>Company notification settings</h1>
-                            <div>
-                                <div>
-                                    <p>New user invited</p>
-                                    <h3>Email</h3>
-                                </div>
-                                <div>
-                                    <p>New user added</p>
-                                    <h3>Email</h3>
-                                </div>
-                                <div>
-                                    <p>Payment method changed</p>
-                                    <h3>Email</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="button">
-                            <button>click</button>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
