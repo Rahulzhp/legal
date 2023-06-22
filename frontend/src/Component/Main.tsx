0e7ee5687eea86392d4d0badddcdbd0005883c9f
Rahulzhp <akumari42872@gmail.com>
2023-06-22 18:38:55 +0530

    added json server

--- frontend/src/Component/Main.tsx
+++ frontend/src/Component/Main.tsx
@@ -1,50 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import "./Main.css";
 import logo2 from "../Icons/Pro.png";
+import bell from "../Icons/Group4.png"
 
-import profile from "../Icons/profile_pic.png";
+import profile from "../Icons/Group3.png";
+import axios from 'axios';
 
 const Main = () => {
-    const [isMobile, setIsMobile] = useState(false);
-
-    const [activeIcon, setActiveIcon] = useState("dashboard");
-    const handleIconClick = (icon) => {
-        setActiveIcon(icon);
-    };
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:8080/posts")
+            .then((res) => setData(res.data))
+            .catch((er) => {
+                console.log(er)
+            })
+    }, [])
 
     return (
-        <div className={`Main_container ${isMobile ? "mobile" : ""}`}>
-            <div className={`sidebar ${isMobile ? "mobile" : ""}`}>
+        <div className={`Main_container`}>
+            <div className={`sidebar`}>
                 {/* Sidebar content goes here */}
                 <div className="sidebar-icons">Company Details</div>
             </div>
-            <div className={`body ${isMobile ? "mobile" : ""}`}>
+            <div className={`body`}>
                 <nav
                     className="DashboardNav"
                     style={{
-                        height: "50px",
+                        height: "139px",
                         padding: "30px",
                         display: "flex",
                         justifyContent: "space-between",
                         position: 'fixed',
-                        top: '0'
-                        , width: '100%'
+                        top: '0',
+                        width: '100%',
+                        backgroundColor: "#FFFFFF"
                     }}
                 >
                     <div style={{ display: "flex" }}>
-                        <img src={logo2} alt="" style={{ width: "20%" }} />
+                        <img src={logo2} alt="" style={{ width: "202px", height: "58px", top: "40px", left: "30px" }} />
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <img
-                            src={profile}
-                            alt="porfile"
-                            style={{
-                                width: "25%",
-                                borderRadius: "50%",
-                                border: "1px solid green",
-                            }}
-                        />
-                        <p>Hello John Doe</p>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <div >
+                            <img src={bell} style={{ width: "71px", }}></img>
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                            <img
+                                src={profile}
+                                alt="porfile"
+                                style={{ width: "173px", marginTop: "-5px", }}
+                            />
+
+                        </div>
                     </div>
                 </nav>
                 {/* Body content goes here */}
